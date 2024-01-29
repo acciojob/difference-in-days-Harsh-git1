d336ef8eb8f9394c380df693e6f82575fba2a293
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-29 13:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  //write your code here
+	const val = Date.parse(date1);
+    const val2 = Date.parse(date2);
+    const val3 = 24*60*60*1000;
+    return parseInt((val2 - val)/val3);
+	
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
